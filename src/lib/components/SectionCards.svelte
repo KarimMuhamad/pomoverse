9313b730bb8cc249624997506a27356a438eb09f
Choyim <karimmuhammad551@gmail.com>
2025-09-06 17:02:13 +0700

    Section Card Summary for Statistic Page

---
+++ src/lib/components/SectionCards.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+  import * as Card from "$lib/components/ui/card/index.js";
+  import { Badge } from "$lib/components/ui/badge/index.js";
+  import { TrendingUp, TrendingDown, Minus, Flame, Trophy } from '@lucide/svelte';
+</script>
+
+<div class="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-3">
+  <Card.Root>
+    <Card.Header>
+      <Card.Title class="font-semibold">Focus</Card.Title>
+      <Card.Action>
+        <Badge variant="default">
+          <TrendingUp class="h-4 w-4"/> 20 %
+        </Badge>
+      </Card.Action>
+    </Card.Header>
+    <Card.Content>
+      <p class="text-2xl font-bold">210 Min</p>
+    </Card.Content>
+    <Card.Footer>
+      <p class="text-muted-foreground font-semibold text-sm">Card Footer</p>
+    </Card.Footer>
+  </Card.Root>
+
+  <Card.Root>
+    <Card.Header>
+      <Card.Title class="font-semibold">Session</Card.Title>
+      <Card.Action>
+        <Badge variant="destructive">
+          <TrendingDown class="h-4 w-4"/> 5 %
+        </Badge>
+      </Card.Action>
+    </Card.Header>
+    <Card.Content>
+      <p class="text-2xl font-bold">10x</p>
+    </Card.Content>
+    <Card.Footer>
+      <p class="text-muted-foreground font-semibold text-sm">Card Footer</p>
+    </Card.Footer>
+  </Card.Root>
+
+  <Card.Root>
+    <Card.Header>
+      <Card.Title class="font-semibold">Breaks Taken</Card.Title>
+      <Card.Action>
+        <Badge variant="secondary">
+          <Minus class="h-4 w-4"/> 0 %
+        </Badge>
+      </Card.Action>
+    </Card.Header>
+    <Card.Content>
+      <p class="text-2xl font-bold">4x</p>
+    </Card.Content>
+    <Card.Footer>
+      <p class="text-muted-foreground font-semibold text-sm">Card Footer</p>
+    </Card.Footer>
+  </Card.Root>
+
+  <Card.Root>
+    <Card.Header>
+      <Card.Title class="font-semibold">Consistency</Card.Title>
+      <Card.Action>
+        <Badge variant="secondary" class="bg-amber-600">
+          <Flame class="h-4 w-4"/> 5x
+        </Badge>
+      </Card.Action>
+    </Card.Header>
+    <Card.Content>
+      <p class="text-2xl font-bold">5 Day</p>
+    </Card.Content>
+    <Card.Footer>
+      <p class="text-muted-foreground font-semibold text-sm">Card Footer</p>
+    </Card.Footer>
+  </Card.Root>
+
+  <Card.Root>
+    <Card.Header>
+      <Card.Title class="font-semibold">Top Label</Card.Title>
+      <Card.Action>
+        <Badge variant="default">
+          <Trophy class="h-4 w-4"/> Java
+        </Badge>
+      </Card.Action>
+    </Card.Header>
+    <Card.Content>
+      <p class="text-2xl font-bold">320 Min</p>
+    </Card.Content>
+    <Card.Footer>
+      <p class="text-muted-foreground font-semibold text-sm">Card Footer</p>
+    </Card.Footer>
+  </Card.Root>
+</div>
--- src/routes/(app)/statistic/+page.svelte
+++ src/routes/(app)/statistic/+page.svelte
@@ -1,5 +1,36 @@
 <script lang="ts">
   import {goto} from "$app/navigation";
+  import * as Chart from "$lib/components/ui/chart/index.js";
+  import { scaleBand } from "d3-scale";
+  import { BarChart } from "layerchart";
+  import {MonitorIcon} from "@lucide/svelte";
+  import SectionCards from "$lib/components/SectionCards.svelte";
+
+  const chartData = [
+    { month: "January", desktop: 186, mobile: 80 , tablet: 100},
+    { month: "February", desktop: 305, mobile: 200, tablet: 150 },
+    { month: "March", desktop: 500, mobile: 338, tablet: 200 },
+    { month: "April", desktop: 73, mobile: 190, tablet: 250 },
+    { month: "May", desktop: 209, mobile: 130, tablet: 200 },
+    { month: "June", desktop: 214, mobile: 140, tablet: 200 },
+  ];
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "#2563eb",
+      icon: MonitorIcon,
+    },
+    mobile: {
+      label: "Mobile",
+      color: "#60a5fa",
+    },
+    tablet: {
+      label: "Tablet",
+      color: "#96c5fd",
+    },
+  } satisfies Chart.ChartConfig;
+
   let { data } = $props();
 
   if (data.user === null) {
@@ -7,4 +38,46 @@
   }
 </script>
 
-<h1>Coming Soon...</h1>
+
+<div class="flex flex-col gap-2">
+  <div class="flex flex-1 justify-center">
+    <SectionCards/>
+  </div>
+<!--  <Chart.Container config={chartConfig} class="w-full">-->
+<!--    <BarChart-->
+<!--      data={chartData}-->
+<!--      xScale={scaleBand().padding(0.35)}-->
+<!--      x="month"-->
+<!--      axis="x"-->
+<!--      seriesLayout="group"-->
+<!--      legend-->
+<!--      series={[-->
+<!--      {-->
+<!--        key: "desktop",-->
+<!--        icon: MonitorIcon,-->
+<!--        label: chartConfig.desktop.label,-->
+<!--        color: chartConfig.desktop.color-->
+<!--      },-->
+<!--      {-->
+<!--        key: "mobile",-->
+<!--        label: chartConfig.mobile.label,-->
+<!--        color: chartConfig.mobile.color-->
+<!--      },-->
+<!--      {-->
+<!--        key: "tablet",-->
+<!--        label: chartConfig.tablet.label,-->
+<!--        color: chartConfig.tablet.color-->
+<!--      }-->
+<!--    ]}-->
+<!--      props={{-->
+<!--      xAxis: {-->
+<!--        format: (d) => d.slice(0, 3),-->
+<!--      }-->
+<!--    }}-->
+<!--    >-->
+<!--      {#snippet tooltip()}-->
+<!--        <Chart.Tooltip/>-->
+<!--      {/snippet}-->
+<!--    </BarChart>-->
+<!--  </Chart.Container>-->
+</div>
